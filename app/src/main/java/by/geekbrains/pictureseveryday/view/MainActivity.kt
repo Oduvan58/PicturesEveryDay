f54f3bb97c29a0432f4f5f0875259bcbe612cb77
Oduvan58 <yancheuskipavel@gmail.com>
2022-05-16 19:43:27 +0300

    Create two themes and add settings screen for changing themes. Create styles and apply them

--- app/src/main/java/by/geekbrains/pictureseveryday/view/MainActivity.kt
+++ app/src/main/java/by/geekbrains/pictureseveryday/view/MainActivity.kt
@@ -4,6 +4,8 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import by.geekbrains.pictureseveryday.R
 import by.geekbrains.pictureseveryday.databinding.ActivityMainBinding
+import by.geekbrains.pictureseveryday.view.details.SETTINGS_SHARED_PREFERENCES
+import by.geekbrains.pictureseveryday.view.details.THEME_RES_ID
 import by.geekbrains.pictureseveryday.view.main.MainPictureFragment
 
 class MainActivity : AppCompatActivity() {
@@ -12,6 +14,10 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setTheme(
+            getSharedPreferences(SETTINGS_SHARED_PREFERENCES, MODE_PRIVATE)
+                .getInt(THEME_RES_ID, R.style.AppTheme)
+        )
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         if (savedInstanceState == null) {

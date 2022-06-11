2107172a952809a11330b24bdd36eea046498455
Oduvan58 <yancheuskipavel@gmail.com>
2022-06-11 20:13:12 +0300

    Add animation for splash screen

--- app/src/main/java/by/geekbrains/pictureseveryday/view/splash/SplashActivity.kt
+++ app/src/main/java/by/geekbrains/pictureseveryday/view/splash/SplashActivity.kt
@@ -1,31 +1,41 @@
 package by.geekbrains.pictureseveryday.view.splash
 
+import android.animation.Animator
+import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Bundle
-import android.os.Handler
+import android.view.animation.AccelerateDecelerateInterpolator
 import androidx.appcompat.app.AppCompatActivity
-import by.geekbrains.pictureseveryday.R
+import by.geekbrains.pictureseveryday.databinding.ActivitySplashBinding
 import by.geekbrains.pictureseveryday.view.MainActivity
 
+@SuppressLint("CustomSplashScreen")
 class SplashActivity : AppCompatActivity() {
 
-    var handler = Handler()
+    private lateinit var binding: ActivitySplashBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_splash)
+        binding = ActivitySplashBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         showSplashScreen()
     }
 
     private fun showSplashScreen() {
-        handler.postDelayed({
-            startActivity(Intent(this@SplashActivity, MainActivity::class.java))
-            finish()
-        }, 3000)
-    }
+        binding.splashImageView.animate().rotationBy(3000f)
+            .setInterpolator(AccelerateDecelerateInterpolator()).setDuration(5000)
+            .setListener(object : Animator.AnimatorListener {
+                override fun onAnimationStart(animation: Animator?) {}
 
-    override fun onDestroy() {
-        handler.removeCallbacksAndMessages(null)
-        super.onDestroy()
+                override fun onAnimationEnd(animation: Animator?) {
+                    startActivity(Intent(this@SplashActivity, MainActivity::class.java))
+                    finish()
+                }
+
+                override fun onAnimationCancel(animation: Animator?) {}
+
+                override fun onAnimationRepeat(animation: Animator?) {}
+
+            })
     }
 }
